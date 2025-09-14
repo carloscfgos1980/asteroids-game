a9a58f158e7c87e5787a725a6ae9d83f03a2b163
Carlos Infante <kuva5008@gmail.com>
2025-09-14 16:21:57 +0200

    voving around

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 import pygame
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        screen.fill((0, 0, 0))
+
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000  # Limit the frame rate to 60 FPS
 
 
 if __name__ == "__main__":
